d29744a71daa2bd0bc7441f82633f7b61fa4d0ee
rterminus <luizrenanpontes@gmail.com>
2026-07-18 18:57:19 -0300

    chore(db): exchanged sqlite to better-sqlite3

--- backend/src/db/database.ts
+++ backend/src/db/database.ts
@@ -1,25 +1,18 @@
-import sqlite3 from "sqlite3";
+import database from "better-sqlite3";
 import path from "path";
 
 const dbPath = path.resolve(__dirname, "../../tracker.db");
 
-sqlite3.verbose();
+const db = new database(dbPath, { verbose: console.log });
+console.log("Connected to SQLite database.");
 
-const db = new sqlite3.Database(dbPath, (err: Error | null) => {
-  if (err) {
-    console.error("Error connecting to database: ", err);
-  } else {
-    console.log("Connected to SQLite database.");
-
-    db.run(`
-      CREATE TABLE IF NOT EXISTS targets (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        ip TEXT NOT NULL,
-        status TEXT NOT NULL,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-      )
-    `);
-  }
-});
+db.exec(`
+  CREATE TABLE IF NOT EXISTS targets (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ip TEXT NOT NULL,
+    status TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  )
+`);
 
 export default db;

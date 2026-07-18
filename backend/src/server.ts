fc04bde1c88e5a74836db6a977df55d5c529918b
rterminus <luizrenanpontes@gmail.com>
2026-07-18 18:57:44 -0300

    feat(server): added POST route and data treatment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,10 +7,28 @@ import db from "./db/database";
 const app = express();
 app.use(express.json());
 
-app.get("/ping", (req: Request, res: Response) => {
-  res.json({ message: "pong!" });
+// schema for validation
+const targetSchema = z.object({
+  ip: z.ipv4(),
+  status: z.enum(["Recon", "Rooted", "Abandoned"]),
 });
 
+// crud
+app.post("/db", (req: Request, res: Response) => {
+  const validation = targetSchema.safeParse(req.body);
+
+  if (!validation.success) {
+    return res.status(400).json(validation.error);
+  }
+
+  const data = validation.data;
+  const insert = db.prepare("INSERT INTO targets (ip, status) VALUES (?, ?)");
+  insert.run(data.ip, data.status);
+
+  return res.status(201).json({ message: "Target saved successfully." });
+});
+
+// port listening
 app.listen(3000, () => {
   console.log("Server running on port 3000!");
 });

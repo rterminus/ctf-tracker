33c20139d950bcb4d8d2478c8a68713eef702575
rterminus <luizrenanpontes@gmail.com>
2026-07-24 22:19:36 -0300

    fix: added border class

--- frontend/src/components/Panel.tsx
+++ frontend/src/components/Panel.tsx
@@ -2,8 +2,8 @@ import type { ReactNode } from "react";
 
 const Panel = ({ title, children }: { title: string; children: ReactNode }) => {
   return (
-    <div className="flex h-full flex-col gap-6 px-10 py-8 m-8 bg-surface border-outline rounded-xl">
-      <div className="flex gap-8 items-center">
+    <div className="bg-surface border-outline m-8 flex h-full flex-col gap-6 rounded-xl border px-10 py-8">
+      <div className="flex items-center gap-8">
         <h2 className="text-2xl font-bold text-white">{title}</h2>
         <button className="bg-primary hover:bg-primary-hover rounded-4xl px-6 py-4 text-xl font-bold transition-colors hover:text-white">
           <span className="text-2xl">+  </span> Add New Target

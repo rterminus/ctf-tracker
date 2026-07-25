fbd54c90b93dd5058ebd9d2b2b9eeb21d1b9e6a8
rterminus <luizrenanpontes@gmail.com>
2026-07-25 18:37:53 -0300

    feat: not found page added for non-routed pages

--- frontend/src/NotFoundPage.tsx
+++ frontend/src/NotFoundPage.tsx
@@ -1,5 +1,9 @@
 const NotFoundPage = () => {
-  return <></>;
+  return (
+    <div className="bg-surface border-outline m-8 flex flex-col gap-6 h-full rounded-xl border px-10 py-8">
+      <p className="text-text-primary text-4xl font-extrabold text-center">Page not found.</p>
+    </div>
+  );
 };
 
 export default NotFoundPage;
